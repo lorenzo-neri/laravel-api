d4195fa1751674bc42e088af0dfb1764dfd63e95
lorenzo-neri <lorenzo21neri@gmail.com>
2023-11-20 16:55:06 +0100

    add method show in Admin\ProjectController and route in api.php

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -15,4 +15,23 @@ class ProjectController extends Controller
             'result' => Project::with('type', 'technologies')->paginate(10)
         ]);
     }
+
+
+    public function show($slug)
+    {
+
+
+        $project = Project::with('type', 'technologies')->where('slug', $slug)->first();
+        if ($project) {
+            return response()->json([
+                'success' => true,
+                'result' => $project
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'result' => 'Ops! Page not found'
+            ]);
+        }
+    }
 }
