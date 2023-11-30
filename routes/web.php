7a81254c3f6609c69f58471243e814fa740985cf
lorenzo-neri <lorenzo21neri@gmail.com>
2023-11-30 20:25:30 +0100

    remove route welcome

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
-});
+}); */
 
 
 /* Admin routes */
+
 Route::middleware(['auth', 'verified']) #per utenti loggati e verificati
     ->prefix('admin') #prefix degli url iniziano con '/admin/'
     ->name('admin.') #nome delle rotte inizia con 'admin.'
